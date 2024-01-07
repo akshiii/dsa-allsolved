6c3c6872699b73a1a39eafd5cb75ca5ade75437e
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-01-07 23:41:56 +0530

    Doubly Linked List

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,44 @@
+public class DoublyLinkedList {
+    Node head;
+
+    class Node{
+        String data;
+        Node prev;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public void addHead(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void printDLL(){
+        Node curr = head;
+        while (curr.next != null) {
+            System.out.print(curr.data + "-> ");
+            curr = curr.next;
+        }
+        System.out.print(curr.data + "-> NULL");
+    }
+
+
+    public static void main(String[] args) {
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList(); 
+        doublyLinkedList.addHead("2");
+        doublyLinkedList.addHead("3");
+        doublyLinkedList.addHead("4");
+        doublyLinkedList.printDLL();
+    }
+}
