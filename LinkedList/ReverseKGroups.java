ed0a8af1b2e07b758db94f31a922116552c3df4f
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-02-11 22:04:19 +0530

    Reverse LL in K groups

---
+++ LinkedList/ReverseKGroups.java
@@ -0,0 +1,48 @@
+public class ReverseKGroups {
+    
+    public Node kReverse(LinkedList list,Node main, int k) {
+        //base call
+        if(main == null){
+            return null;
+        }
+        //reverse first k nodes
+        Node next = null;
+        Node curr = main;
+        Node prev = null;
+        int count = 0;
+
+        while(curr!= null && count < k){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+            count++;  
+        }
+
+        //recursive call
+        if(next != null){
+            main.next = kReverse(list, next, k);
+        }
+
+        //return head of reversed list
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.addLast("4");
+        list.addLast("5");
+        list.addLast("6");
+        list.printList();
+        System.out.println();
+
+        ReverseKGroups rll = new ReverseKGroups();
+        list.head = rll.kReverse(list,list.head, 2);
+        
+        list.printList();
+    }
+}
