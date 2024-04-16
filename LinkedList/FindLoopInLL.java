8abd293c4acb2fbc8986df27918d5e9c27aa199d
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-04-16 09:54:05 +0530

    Finding loop in LL

---
+++ LinkedList/FindLoopInLL.java
@@ -0,0 +1,41 @@
+// IMP!! -> Here the logic for finding loop is correct but the way we are trying to make a Circular LL is not correct, thats why it may not work here.
+// This is solved in leetcode here-https://leetcode.com/problems/linked-list-cycle/
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindLoopInLL {
+    boolean isLoopExist(LinkedList list){
+        Map<Node,Boolean> visited = new HashMap<>();
+        Node temp = list.head;
+
+        if(list.head == null){
+            return false;
+        }
+        while(temp.next != null){
+            if(visited.get(temp) != null && visited.get(temp) == true){
+                return true;
+            }
+            System.out.println(visited.get(temp));
+            visited.put(temp, true);
+            temp = temp.next;
+        }
+
+        return false;
+    }
+    
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast("1");
+        list.addLast("2");
+        list.addLast("3");
+        list.addLast("4");
+        list.addLast("2");
+
+        list.printList();
+        System.out.println();
+
+        FindLoopInLL findLoop = new FindLoopInLL();
+        System.out.println("Loop in LL ? = "+findLoop.isLoopExist(list));
+    }
+}
