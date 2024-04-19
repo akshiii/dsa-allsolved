3bf5914fb3277e8bcc5125f34e2d46f06dd77005
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-04-19 23:49:13 +0530

    Solved!!!

--- LinkedList/RemoveDuplicates.java
+++ LinkedList/RemoveDuplicates.java
@@ -1,8 +1,4 @@
 //Removing duplicates from Sorted Linked List
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class RemoveDuplicates {
 
     //Removing only duplicate extra nodes
@@ -23,33 +19,87 @@ public class RemoveDuplicates {
         }
     }
 
-    // TODO: Unsolved!!!
+    //Delete all nodes that have duplicate numbers, 1->2->2->3->3->4-> null   Result= 1->4->null
     void deleteAllDuplicates(LinkedList list){
-        Map<Node,Boolean> visited = new HashMap<>();
+        Node lastUniqueNode = null;
         Node curr = list.head;
-        while(curr != null){
-            visited.put(curr, true);
-            curr = curr.next;
+        boolean temp = false;
+        if(list.head == null){
+            return;
+        }
+        if(curr.next == null){
+            return;
+        }
+        if(curr.data != curr.next.data){
+            list.head = curr;
+            lastUniqueNode = curr;
+            temp = true;
+        }
+        while(curr !=null){
+            if(curr.next != null && curr.next.next != null){
+                if(curr.data != curr.next.data && curr.next.data != curr.next.next.data){
+                    if(temp == false){
+                        curr = curr.next;
+                        list.head = curr;
+                        lastUniqueNode = curr;
+                        temp = true;
+                    }
+                    else{
+                        curr = curr.next;
+                        lastUniqueNode.next = curr;
+                        lastUniqueNode = curr;
+                    }
+                } 
+                else{
+                    curr = curr.next;
+                }
+            }
+            else{
+                if(curr.next == null){
+                    if(lastUniqueNode != null){
+                        lastUniqueNode.next = null;
+                    }
+                    else{
+                        list.head = null;
+                    }
+                    break;
+                }
+                else if(curr.next.next == null){
+                    if(lastUniqueNode == null && curr.data == curr.next.data){
+                        list.head = null;
+                    }
+                    else if(lastUniqueNode != null && curr.data == curr.next.data){
+                        lastUniqueNode.next = null;
+                    }
+                    else if(lastUniqueNode == null && curr.data != curr.next.data){
+                        list.head = curr.next;
+                    }
+                    else{
+                        lastUniqueNode.next = curr.next;
+                        break;
+                    }
+                    break;
+                }
+            }
+            
         }
     }
-
-   
     
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.addLast("1");
-        list.addLast("1");
-        list.addLast("1");
-        list.addLast("2");
+        // list.addLast("1");
+        // list.addLast("1");
         // list.addLast("2");
         // list.addLast("2");
-        list.addLast("3");
+        // list.addLast("2");
+        // list.addLast("3");
         // list.addLast("3");
         // list.addLast("4");
-        list.addLast("4");
+        // list.addLast("4");
         // list.addLast("5");
-        list.addLast("5");
+        // list.addLast("5");
         list.printList();
 
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
---
+++ LinkedList/SplitLLInHalf.java
@@ -0,0 +1,10 @@
+//We have 2 circular LL 
+//  odd LL -> 1->2->3->4->5->1
+//make 2 LL    1->2>3->1 && 4->5->4
+
+
+//  EVEN LL -> 1->2->3->4->1
+//make 2 LL    1->2->1 && 3->4->3
+public class SplitLLInHalf {
+     
+}
