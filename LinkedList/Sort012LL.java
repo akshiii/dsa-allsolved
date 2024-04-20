a970ccca306491db4940c706c2d92c2e8cdee876
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-04-20 13:55:38 +0530

    Sorting into 0 1 2 done

--- LinkedList/Sort012LL.java
+++ LinkedList/Sort012LL.java
@@ -1,6 +1,7 @@
 //Sorting 0s, 1s, 2s in LL
 public class Sort012LL {
 
+    //Approach 1
     void sortLL(LinkedList list){
         int zeroCount = 0;
         int oneCount = 0;
@@ -41,6 +42,55 @@ public class Sort012LL {
         }
     }
 
+    void insertAtTail(Node tail, Node curr){
+        tail.next = curr;
+        tail = curr;
+    }
+    //Approach 2  
+    void sortLL2(LinkedList list, Sort012LL sort012ll){
+        Node zeroHead = new Node("-1");
+        Node zeroTail = zeroHead;
+
+        Node oneHead = new Node("-1");
+        Node oneTail = oneHead;
+        
+        Node twoHead = new Node("-1");
+        Node twoTail = twoHead;
+
+        Node curr = list.head;
+        //creating separate lists fro 0 1 and 2
+        while(curr!= null){
+
+            if(curr.data == "0"){
+                zeroTail.next = curr;
+                zeroTail = curr;
+            }
+            else if (curr.data == "1") {
+                oneTail.next = curr;
+                oneTail = curr;
+            }
+            else if(curr.data == "2"){
+                twoTail.next = curr;
+                twoTail = curr;
+            }
+             
+            curr = curr.next;
+        }
+
+        //Now merge all 3 lists
+        if(oneHead.next != null){
+            zeroTail.next = oneHead.next;
+        }
+        else {
+             zeroTail.next = twoHead.next;
+        }
+        
+        oneTail.next = twoHead.next;
+        twoTail.next = null;
+        list.head = zeroHead.next;
+        
+
+    }
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.addLast("1");
@@ -52,7 +102,7 @@ public class Sort012LL {
         list.printList();
 
         Sort012LL sort012ll = new Sort012LL();
-        sort012ll.sortLL(list);
+        sort012ll.sortLL2(list,sort012ll);
         System.out.println("");
         list.printList();
     }
