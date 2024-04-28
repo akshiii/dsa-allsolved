c82e24074c09cc9d8807231b176cc95042a5a6d5
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-04-28 19:31:16 +0530

    N stacks Problem

--- Stacks/NStacksProblem.java
+++ Stacks/NStacksProblem.java
@@ -1,9 +1,7 @@
 package Stacks;
 
-//Storing N stacks in a single array
-public class NStacksProblem {
-
-    class NStacks{
+//Design a class which stores N stacks in a single array
+class NStacks{
         int n;
         int s;
         int[] arr;
@@ -38,10 +36,65 @@ public class NStacksProblem {
         }
 
         // x is value, m is stack
-        void push(int x,int m){
+        boolean push(int x,int m){
+            if(freespot == -1){
+                //no free space present
+                return false;
+            }
+
             //find index
-            
+            int index = this.freespot;
+
+            //update freespot
+            this.freespot = this.next[index];
+
+            //insert in arr
+            this.arr[index] = x;
+
+            //Update next[]
+            this.next[index] = this.top[m-1];
+
+            //Update top
+            this.top[m-1] = index;
+
+            return true;
         }
+
+        //Pops top element from mth stack, returns -1 if stack is empty, otherwise returns popped value
+        int pop(int m){
+            if(top[m-1] == -1){
+                return -1;
+            }
+
+            //Push commands in reverse
+            int index = this.top[m-1];
+            this.top[m-1] = this.next[index];
+            this.next[index] = this.freespot;
+            this.freespot = index;
+
+            return this.arr[index];
+
+        }
+}
+
+
+public class NStacksProblem {
+
+     public static void main(String[] args) {
+        NStacks nStacks = new NStacks(3,6);
+        nStacks.push(10,1);
+        nStacks.push(20,1);
+        nStacks.push(30,1);
+        nStacks.push(40,2);
+        nStacks.pop(1);
+        nStacks.push(50,1);
+        nStacks.pop(1);
+
+        for(int i = 0; i< nStacks.arr.length; i++){
+            System.out.print(nStacks.arr[i]+" , ");
+        }
+        
+
     }
     
     
