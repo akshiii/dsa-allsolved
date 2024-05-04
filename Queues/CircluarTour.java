100cf0455b5459b1ae03f1b35bdff5c32aa33b54
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-05-04 15:17:27 +0530

    First non repeating characters

---
+++ Queues/CircluarTour.java
@@ -0,0 +1,16 @@
+package Queues;
+
+//Very Imp question
+public class CircluarTour {
+    // static int findStartingPoint(int petrol[], int distance[]){
+        
+    // }
+    public static void main(String[] args) {
+        // int[] petrol = {4 ,6 ,7 ,4};
+        // int distance[] = {6,5,3,5};
+
+        int[] petrol = {55 ,33 ,77 ,40 };
+        int distance[] = {52 ,100 ,61 ,69 }; // -1 ( not possible)
+        // System.out.println("Starting point = "+findStartingPoint(petrol,distance));
+    }
+}
