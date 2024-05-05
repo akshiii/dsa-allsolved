2ba9f67d9a4e350793f13e945c6eee6c4821b978
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-05-05 11:44:00 +0530

    Binary Trees

--- BinaryTree/BinaryTreeExample.java
+++ BinaryTree/BinaryTreeExample.java
@@ -1,11 +1,25 @@
 package BinaryTree;
 
+
+class node{
+    int data;
+    node left;
+    node right;
+
+    node(int d){
+        this.data = d;
+        this.left = null;
+        this.right = null;
+    }
+}
 class BinaryTree{
 
 }
 
 public class BinaryTreeExample {
     public static void main(String[] args) {
-        
+        node root = new node(1);
+
+
     }
 }
