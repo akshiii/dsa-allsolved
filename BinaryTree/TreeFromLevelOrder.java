98179db686b438d91de2a9ac538fb4dd1b916990
akshita.tyagi <akshita.tyagi@ril.com>
2024-06-12 09:41:41 +0530

    Tree from Level order Trav

---
+++ BinaryTree/TreeFromLevelOrder.java
@@ -0,0 +1,13 @@
+package BinaryTree;
+
+//Making a tree from Level Order traversal
+public class TreeFromLevelOrder {
+
+    static void makeTree(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
