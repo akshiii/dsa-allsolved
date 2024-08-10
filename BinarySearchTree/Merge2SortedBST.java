38ca732976cc5dc249d5a3843f1267469d480c16
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-08-10 16:27:27 +0530

    approach 2 merge 2 bst

--- BinarySearchTree/Merge2SortedBST.java
+++ BinarySearchTree/Merge2SortedBST.java
@@ -2,63 +2,27 @@ package BinarySearchTree;
 
 import java.util.ArrayList;
 
-//TODO: Unsolved
-////This approach has TC= O(n) and SC= O(1)
+//This question solved 3 hard questions
+//This approach has TC= O(n) and SC= O(1)
 public class Merge2SortedBST {
     static ArrayList<Integer> arr = new ArrayList<>(); 
-    static node prev;
+    static node prev = null;
 
-    //converting a bst into a sorted doubly linked list
-    // static void convertIntoSortedDLL(node root, node head){
-    //     if(root == null){
-    //         return;
-    //     }
-
-    //     convertIntoSortedDLL(root.right, head);
-    //     root.right = head;
-    //     if(head != null){
-    //         head.left = root;
-    //     }
-    //     head = root;
-
-    //     convertIntoSortedDLL(root.left, head);
-    // }
-
-    static void Inorder(node curr){
-        // Base case
-        if (curr == null)
+    //converting a bst into a sorted doubly linked list in-place
+    static void convertIntoSortedDLL(node root){
+        if(root == null){
             return;
-        Inorder(curr.left);
-        prev.left = null;
-        prev.right = curr;
-        prev = curr;
-        Inorder(curr.right);
-    }
-  
-    // Function to flatten binary
-    // tree using level order
-    // traversal
-    static node flatten(node parent){
-        // Dummy node
-        node dummy = new node(-1);
-        
-        // Pointer to previous
-        // element
-        prev = dummy;
-        
-        // Calling in-order
-        // traversal
-        Inorder(parent);
-        
-        prev.left = null;
-        prev.right = null;
-        node ret = dummy.right;
-        
-        // Delete dummy node
-        //delete dummy;
-        return ret;
+        }
+
+        convertIntoSortedDLL(root.left);
+        prev.right = root;
+        root.left = prev;
+        prev = root;
+
+        convertIntoSortedDLL(root.right);
     }
 
+    //merging 2 linked lists into one sorted ll
     static node merge2LL(node head1, node head2){
         node head = null;
         node tail = null;
@@ -106,41 +70,39 @@ public class Merge2SortedBST {
         return head;
     }
 
-    static int countNodes(node root){
-        int count = 0;
-        node temp = root;
-        while(temp!= null){
-            count++;
-            temp = temp.right;
+    //Uses slow ans fast method to give mid node in a ll
+    static node getMidNode(node root){
+        if(root == null || root.right == null){
+            return root;
         }
-        return count;
+        node slow = root;
+        node fast = root;
+        while(fast.right != null && fast.right.right != null){
+            fast = fast.right.right;
+            slow = slow.right;
+        }
+        return slow;
     }
 
-    static node sortedLLToBst(node head, int n ){
-        //base case
-        if(n <= 0 || head == null){
-            return null;
+    //converts linked list to a bst
+    static node llToBst(node head){
+        if(head == null || head.right == null){
+            return head;
+        }
+        node midNode = getMidNode(head);
+        node prev = midNode.left;
+        node forw = midNode.right;
+
+        forw.left = midNode.left = midNode.right = null;
+        if(prev != null){
+            prev.right = null;
         }
 
-        node left = sortedLLToBst(head, n/2);
-        node root = head;
-        root.left = left;
-        head = head.right;
-
-        root.right = sortedLLToBst(head, n - n/2 -1);
-        return root;
+        midNode.left = llToBst(prev==null ? null : head);
+        midNode.right = llToBst(forw);
+        return midNode;
     }
 
-    
-    
-    // static void InOrder(node root){
-    //     if(root == null){
-    //         return;
-    //     }
-    //     InOrder(root.left);
-    //     System.out.print(root.data+ " ");
-    //     InOrder(root.right);
-    // }
     public static void main(String[] args) {
         node root1 = new node(5);
         root1.left = new node(3);
@@ -153,28 +115,27 @@ public class Merge2SortedBST {
         root2.right = new node(6);
         root2.right.right = new node(7);
 
-        //merge 2 bst
-        //Step 1 : flatten both bst into sorted linked lists
-        // node head1 = null;
-        node head1 = flatten(root1);
-        head1.left = null;
+        //Step 1 : flatten both bst into sorted doubly linked lists
+        //BST 1:
+            node dummy = new node(-1);
+            prev = dummy;
+            convertIntoSortedDLL(root1);
+            node head1 = dummy.right;
+            dummy.right = head1.left = null;
+       
+        //BST 2:
+            dummy = new node(-1);
+            prev = dummy;
+            convertIntoSortedDLL(root2);
+            node head2 = dummy.right;
+            dummy.right = head2.left = null;
         
-
-        node head2 = flatten(root2);
-        // convertIntoSortedDLL(root2, head2);
-        head2.left = null;
-
-        // flatten(root1);
-        // flatten(root2);
-
-        // InOrder(root1);
         //Step 2: Merge 2 sorted linked lists
         node head = merge2LL(head1, head2);
 
         //Step 3: Convert sorted ll into BST
-        node bst = sortedLLToBst(head, countNodes(head));
+        node bst  = llToBst(head);
 
-        System.out.println("New bst = ");
-        // InOrder(bst);
+        System.out.println("New bst = "+bst.data);
     }
 }
