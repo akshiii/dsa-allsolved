9757932b5f98d9d6a3dba8c27dfa76abd73f5155
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-08-30 00:40:05 +0530

    ts

--- Graphs/TopologicalSort.java
+++ Graphs/TopologicalSort.java
@@ -1,6 +1,52 @@
 package Graphs;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 //uisng dfs
 public class TopologicalSort {
-    
+    static void topologicalSort(Map<Integer, Boolean> visited, int node, Graph<Integer> graph , Stack<Integer> stack){
+        visited.put(node, true);
+
+        if(graph.adj.containsKey(node)){
+            for (int neighbour : graph.adj.get(node)) {
+                if(visited.get(neighbour) == false){
+                    topologicalSort(visited, neighbour, graph, stack);
+                }
+            }
+        }
+        stack.push(node);
+        
+    }
+    public static void main(String[] args) {
+        int vertex = 6; // no of nodes
+        int startingNode = 1; // starting node
+        Map<Integer , Boolean> visited = new HashMap<>();
+        Graph<Integer> graph = new Graph<>();
+        graph.addEdge(1, 2, true);
+        graph.addEdge(1, 3, true);
+        graph.addEdge(2, 4, true);
+        graph.addEdge(3, 4, true);
+        graph.addEdge(4, 6, true);
+        graph.addEdge(4, 5, true);
+        graph.addEdge(5, 6, true);
+        graph.printAdjList();
+
+        for(int i = 0; i < vertex; i++){
+            visited.put(i+startingNode, false);
+        }
+
+        System.out.print("Topological sort => ");
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < vertex; i++){
+            if(visited.get(i+startingNode) == false){
+                topologicalSort(visited, startingNode, graph, stack);
+            }
+        }
+
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop() + " ");
+        }
+    }
 }
