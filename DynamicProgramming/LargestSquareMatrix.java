aa61617b7e118e90b56fe09fbb3be42b1b5e452a
akshita.tyagi <akshita.tyagi@ril.com>
2024-10-15 00:36:19 +0530

    largest square matrix

--- DynamicProgramming/LargestSquareMatrix.java
+++ DynamicProgramming/LargestSquareMatrix.java
@@ -21,9 +21,97 @@ public class LargestSquareMatrix {
 		else
 			return 0;
 	}
+
+	static int memoizedSol(int[][] matrix, int i, int j, int[][] dp){
+		if(i >= matrix.length || j >= matrix[0].length){
+			return 0;
+		}
+
+		if(dp[i][j] != -1){
+			return dp[i][j];
+		}
+
+		int right = memoizedSol(matrix, i, j+1,dp);
+		int down = memoizedSol(matrix, i+1, j,dp);
+		int diagonal = memoizedSol(matrix, i+1, j+1,dp);
+
+		if(matrix[i][j] == 1){
+			dp[i][j] = 1 + Math.min(right, Math.min(down, diagonal));
+			max = Math.max(max, dp[i][j]);
+			return dp[i][j];
+		}
+		else
+			return dp[i][j] = 0;
+	}
+
+	static void tabulationSol(int[][] matrix){
+		int row = matrix.length;
+		int column = matrix[0].length;
+
+		int[][] dp = new int[row+1][column+1];
+		for (int i = row -1; i >= 0; i--) {
+			for(int j = column -1; j >= 0 ; j--){
+
+				int right = dp[i][j+1];
+				int down = dp[i+1][j];
+				int diagonal = dp[i+1][j+1];
+
+				if(matrix[i][j] == 1){
+					dp[i][j] = 1 + Math.min(right, Math.min(down, diagonal));
+					max = Math.max(max, dp[i][j]);
+				}
+				else
+					dp[i][j] = 0;
+			}
+		}
+	}
+
+	static void spaceOptimizedSol(int[][] matrix){
+		int row = matrix.length;
+		int column = matrix[0].length;
+
+		int[] curr = new int[column+1];
+		int[] next = new int[column+1];
+
+		for (int i = row -1; i >= 0; i--) {
+			for(int j = column -1; j >= 0 ; j--){
+
+				int right = curr[j+1];
+				int down = next[j];
+				int diagonal =next[j+1];
+
+				if(matrix[i][j] == 1){
+					curr[j] = 1 + Math.min(right, Math.min(down, diagonal));
+					max = Math.max(max, curr[j]);
+
+				}
+				else
+					curr[j] = 0;
+			}
+			next = curr;
+		}
+	}
 	public static void main(String[] args) {
 		int[][] matrix = {{1,1,1,1,1},{1,1,1,1,1},{0,1,1,1,0}}; //3
 		maxArea(matrix, 0,0);
+		System.out.println("Max area = "+ max);
+
+		max = 0;
+		int[][] dp = new int[matrix.length][matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++){
+				dp[i][j] = -1;
+			}
+		}
+		memoizedSol(matrix, 0,0, dp);
+		System.out.println("Max area = "+ max);
+
+		max = 0;
+		tabulationSol(matrix);
+		System.out.println("Max area = "+ max);
+
+		max = 0;
+		spaceOptimizedSol(matrix);
 		System.out.println("Max area = "+ max);
 	}
 }
