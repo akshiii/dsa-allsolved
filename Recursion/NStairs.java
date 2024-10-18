de2d418e9d91f1e4454f5368e0d7c81341d34d15
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-10-19 00:35:51 +0530

    N stairs

---
+++ Recursion/NStairs.java
@@ -0,0 +1,20 @@
+public class NStairs {
+
+    static int distinctWays(int stairs){
+        if(stairs < 0){
+            return 0;
+        }
+
+        if(stairs == 0){
+            return 1;
+        }
+
+        int ways = distinctWays(stairs-1) + distinctWays(stairs-2);
+        return ways;
+    }
+    public static void main(String[] args) {
+        int stairs = 3;
+        System.out.println(distinctWays(stairs));
+
+    }
+}
